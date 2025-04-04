5df6dd9a75a1de724d761eb58f5dede3cca89969
fr4ncisUCM <francisco.calvo.gonz@gmail.com>
2025-04-04 23:36:17 +0200

    f

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Cambiar al directorio de la aplicación
-cd /var/www/flask-app
-# Iniciar Gunicorn en segundo plano
-sudo gunicorn --bind 0.0.0.0:5000 app:app --daemon --workers=2
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
