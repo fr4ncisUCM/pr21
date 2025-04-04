cc0c1dd0a053342c8dd07b1fb0d466123949cf71
fr4ncisUCM <87377072+fr4ncisUCM@users.noreply.github.com>
2025-04-04 19:39:42 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-TXT_FILE="/home/ubuntu/deploy-test/mensaje.txt"
-
-if [ -f "$TXT_FILE" ]; then
-    echo "✅ El archivo mensaje.txt ha sido desplegado correctamente."
-else
-    echo "❌ ERROR: No se encontró mensaje.txt en /home/ubuntu/deploy-test/"
-    exit 1  # Retorna error si el archivo no está
-fi
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
